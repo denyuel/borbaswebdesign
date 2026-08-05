e95878c6fb6e9e037461d4486e5b4739827c3866
Daniel Borbas <denyuel@gmail.com>
2026-08-05 17:22:29 +0200

    feat: link the live Borbás Webdesign Facebook page in the SocialLinks component

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -14,15 +14,13 @@ export default function SocialLinks() {
                 daniel.borbas@borbaswebdesign.hu
             </a>
             
-            {/* Facebook Link (Placeholder) */}
+            {/* Facebook Link */}
             <a 
-                href="#" 
+                href="https://www.facebook.com/profile.php?id=61593077453422" 
                 className="social-link" 
-                title="Kövess Facebookon! (Hamarosan)"
-                onClick={(e) => {
-                    e.preventDefault();
-                    alert("A Facebook oldal hamarosan elérhető!");
-                }}
+                title="Kövess Facebookon!"
+                target="_blank"
+                rel="noopener noreferrer"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
             </a>
